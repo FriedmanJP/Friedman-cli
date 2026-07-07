32eee43cc19fec7ba8b1809e513ce48668993cf1
chung9207 <mirimtl@protonmail.com>
2026-07-07 15:25:03 +0900

    chore: remove tracked precompile_exec.jl (gitignored build artifact)

--- precompile_exec.jl
+++
@@ -1,9 +0,0 @@
-using Friedman
-# Exercise build_app and help paths
-app = Friedman.build_app()
-Friedman.dispatch(app, ["--help"])
-Friedman.dispatch(app, ["estimate", "--help"])
-Friedman.dispatch(app, ["test", "--help"])
-Friedman.dispatch(app, ["irf", "--help"])
-Friedman.dispatch(app, ["forecast", "--help"])
-Friedman.dispatch(app, ["--version"])
